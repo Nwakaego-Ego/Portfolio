f619abc4f535ab1e956762df30d5880b5cee7975
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-09-23 16:31:46 -0700

    updated my cv

--- src/app/header/page.js
+++ src/app/header/page.js
@@ -111,7 +111,7 @@ const header = () => {
             </div>
             <button className="bg-white hover:bg-normalBlue hover:text-white transition duration-300 ease-in-out transform hover:scale-105  text-normalBlue font-bold py-2 px-4 rounded m-10">
               <a
-                href="https://drive.google.com/file/d/1KVsW43K6QYqwHr2wCSQ3JhtkVxFhiMMj/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1xwnvgwYVBIiVJeIFdNB4XLyXj8cJv19g/view?usp=drive_link"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="cursor-pointer"
